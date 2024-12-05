d3f8bb3bb40b4eac557a292a8e75d0199bc404b6
Claus Guttesen <kometen@gmail.com>
2024-12-05 20:57:22 +0000

    rusty-psql: add 0.1.1 bottle.

--- Formula/rusty-psql.rb
+++ Formula/rusty-psql.rb
@@ -5,6 +5,13 @@ class RustyPsql < Formula
   sha256 "7341a60f81b09f25402b6bd768d06c41b45489e3cb50b1e89221181e09cb5ac6"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/kometen/homebrew-rusty-psql/releases/download/rusty-psql-0.1.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "66ce1cb6d8223d13c3d94b5a540621b0b278716a0dcbcf7fddef8426a4b23837"
+    sha256 cellar: :any_skip_relocation, ventura:       "8f4713c59242a715bc80071fad82ac9b13cff7e4fc009768a8f5c6708d093609"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "6aea8eefd129c58866857d6f30e5ac100b87c82cb1462caacff3b255ad27fdb5"
+  end
+
   depends_on "rust" => :build
   depends_on "openssl@3.4"
 
