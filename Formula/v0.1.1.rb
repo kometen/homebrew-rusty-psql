d2b4f9cb93ba3527bf4d67043b27770287822589
Claus Guttesen <kometen@gmail.com>
2024-12-05 20:00:29 +0100

    v0.1.1

---
+++ Formula/v0.1.1.rb
@@ -0,0 +1,28 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class V011 < Formula
+  desc "Wrapper around psql where connection information is pulled from Azure Key Vault"
+  homepage "https://github.com/kometen/rusty-psql"
+  url "https://github.com/kometen/rusty-psql/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "7341a60f81b09f25402b6bd768d06c41b45489e3cb50b1e89221181e09cb5ac6"
+  license "MIT"
+
+  depends_on "rust" => :build
+  depends_on "openssl@3.4"
+
+  def install
+    system "cargo", "install", *std_cargo_args
+
+    # Install binary
+    bin.install "target/release/rusty_psql"
+  end
+
+  test do
+    output = shell_output("#{opt_bin}/rusty_psql --help | /usr/bin/head -1 | tr -d '\n' 2>&1")
+    expected = "Usage: rusty_psql --namespace <NAMESPACE>"
+    assert_match expected, output
+
+    assert_match version.to_s, shell_output("#{opt_bin}/rusty_psql --version")
+  end
+end
